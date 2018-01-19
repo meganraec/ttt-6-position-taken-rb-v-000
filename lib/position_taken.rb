ba09eeb71a4f40892ccf313424d433cee0cf6977
Megan Collier <meganraec@live.com>
2018-01-19 14:27:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,10 +4,10 @@ board = Array.new(9, " ")
 def position_taken?(board, index) do
   if board[index] == " " || board[index] == ""
     puts false
-  #elsif board[index] == nil
-  #  puts false
-  #else board [index] == "X" || "O"
-  #  puts true
+  elsif board[index] == nil
+    puts false
+  else board [index] == "X" || board[array] == "O"
+    puts true
   end
 end
 
