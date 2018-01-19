36a2e944bf40240c6adb1db1148efb36fc56ded3
Megan Collier <meganraec@live.com>
2018-01-19 14:23:35 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+board = Array.new(9, " ")
+
+def position_taken?(board, index) do
+  if board[index] == " " || ""
+    puts false
+  elsif board[index] == nil
+    puts false
+  else board [index] == "X" || "O"
+    puts true
+  end
+end
+
+board = []
+index = 0
